40a6abd7225c727b4c334bc7d1a56762eefa6636
Jeff Gore <jeff.gore@aexp.com>
2020-07-17 11:00:27 -0400

    further refactoring and comments

--- src/main.kt
+++ src/main.kt
@@ -5,28 +5,35 @@ fun constructBaseUrls(baseUrlPortion: String) : MutableList<String> {
     val urlSegments = baseUrlPortion.split("/")
     val optionsBySegment = mutableListOf<List<String>>()
 
+    // An array representing URL segments, within each element is another array of all the options for that segment:
+    // Ex: [[http:],[],[localhost:3000][home, about, contact]]
     for (segment in urlSegments) {
         val segmentOptions = segment.split(",").map{ it.trim() }
         optionsBySegment.add(segmentOptions)
     }
 
-    // Initialize to one string; there should be at least one URL output from this function
+    // Initialize to one string; there should be at least one URL output from this function.
     val urlList = mutableListOf("")
+
     for ((segmentIndex, optionsForSegment) in optionsBySegment.withIndex()) {
         val prefix = if (segmentIndex == 0) "" else "/"
 
-        // Since we
-        val tempUrlList = urlList.toMutableList()
-        for ((optionIndex, option) in optionsForSegment.withIndex()) {
+        // Since we are going to append to the URL list in the process of discovering more options, we need to use a copy
+        // of the pre-loop URL list to avoid a `ConcurrentModificationException`. This is also necessary so that all options
+        // are operating upon the same base.
+        val preOptionsUrlList = urlList.toMutableList()
 
-            // If this is the first option in the list, then just append to all the URLs in the collection.
+        for ((optionIndex, option) in optionsForSegment.withIndex()) {
+            // If this is the first option in the list, then just append to all the URLs in the collection. The URLs
+            // require this segment to be populated.
             if (optionIndex == 0) {
                 for (i in 0 until urlList.size) {
                     urlList[i] += "$prefix$option"
                 }
-            } else { // Otherwise this is a 2nd option which doubles the size of the list. We'll have to add new elements.
-                for (i in 0 until tempUrlList.size) {
-                    urlList.add(tempUrlList[i] + "$prefix$option")
+            } else { // Otherwise this is the 2nd+ option for this URL segment; we'll have to add new URLs.
+                // We'll use our preOptionsUrlList as a basis, which contains the list before the first option was added.
+                for (i in 0 until preOptionsUrlList.size) {
+                    urlList.add(preOptionsUrlList[i] + "$prefix$option")
                 }
             }
         }
@@ -34,21 +41,32 @@ fun constructBaseUrls(baseUrlPortion: String) : MutableList<String> {
     return urlList
 }
 
-fun appendQueryStrings(queryPortion: String, urlList: MutableList<String>) {
+fun appendQueryStrings(queryPortion: String, baseUrlList: MutableList<String>) : MutableList<String> {
+    var urlList = baseUrlList.toMutableList()
+    // Because query keys can have multiple possible values, the values are separated with commas, and the key-value
+    // pairs are separated with semicolons.
     val queryStrings = queryPortion.split(";").toMutableList()
     queryStrings.sort()
-    for (queryString in queryStrings) {
-        val tempUrlList = urlList.toMutableList()
 
-        for (i in 0 until tempUrlList.size) {
+    for (queryString in queryStrings) {
+        // Since we are going to append to the URL list in the process of discovering more options, we need to use a copy
+        // of the pre-loop URL list to avoid a `ConcurrentModificationException`. This is also necessary so that all query
+        // value options are operating upon the same base.
+        val preOptionsUrlList = urlList.toMutableList()
+
+        for (i in 0 until preOptionsUrlList.size) {
             val (queryKey, queryValueString) = queryString.split("=")
             val queryValues = queryValueString.split(",")
+
+            // In this case, we assume that we always want a version of the URL without the query string. So we will
+            // always choose to add a new value to the URL list, rather than mutating the existing value.
             for (queryValue in queryValues) {
-                val prefix = if (tempUrlList[i].contains("?")) "&" else "?"
-                urlList.add(tempUrlList[i] + "$prefix$queryKey=$queryValue")
+                val prefix = if (preOptionsUrlList[i].contains("?")) "&" else "?"
+                urlList.add(preOptionsUrlList[i] + "$prefix$queryKey=$queryValue")
             }
         }
     }
+    return urlList
 }
 
 data class MalformedURL(val url: String, val errReason: String?)
@@ -82,12 +100,13 @@ fun printResults(urlList: MutableList<String>, invalidUrlList: MutableList<Malfo
 }
 
 fun main(args: Array<String>) {
-//    var args = listOf("https://www.americanexpress.com/en-us/loans/apply/personal-loan,business-loan?pcn=hello;login=simple,card")
+    // // Shadowed args to facilitate local testing:
+    // var args = listOf("http://localhost:3000/home,about,contact,store?promo=friend,bff,enemy;goodUx=true")
     val joinedArgs = args.joinToString(separator = "")
     val (baseUrlPortion, queryPortion) = joinedArgs.split( "?")
 
-    val urlList = constructBaseUrls(baseUrlPortion)
-    appendQueryStrings(queryPortion, urlList)
+    val baseUrlList = constructBaseUrls(baseUrlPortion)
+    val urlList = appendQueryStrings(queryPortion, baseUrlList)
     val invalidUrlList = removeInvalidUrls(urlList)
     printResults(urlList, invalidUrlList)
 }
